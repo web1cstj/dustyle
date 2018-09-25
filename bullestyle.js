a5dd6707392c77808bb61ad2d765a1819a59b38a
Martin Boudreau <mboudrea@cstj.qc.ca>
2018-09-25 13:49:17 -0400

    fonctionnel

--- bullestyle.js
+++ bullestyle.js
@@ -1,54 +1,106 @@
-$(function() {
-	  $('head').append('<link rel="stylesheet" href="bullestyle.css" type="text/css" />');
-		$("*[style]").each(function(){
-		var $this = $(this);
-		var $bulle = $(document.createElement('div'))
-			.addClass("bulle")
-		;
-		var style = $this.attr("style");
-		style = style.replace(/^ */g, "").replace(/ *$/g, "").replace(/ *: */g, ":").replace(/ *; */g, ";").replace(/ *\/\* */g, "/*").replace(/ *\*\/ */g, "*/").replace(/;+/g, ";");
-		style = style.split(";");
-		var nouveau = true;
-		var supprimer = false;
-		var cacher = false;
-		var nbProps = 0;
-		for (var i=0; i<style.length; i++) {
-			var s = style[i];
-			if (s.substr(0,2) == "*/") {
-				supprimer=false;
-				s = s.substr(2);
-			}
-			while (s.substr(0,4) == "/**/") {
-				if (i==0 || nouveau==false) cacher=true;
-				nouveau=false;
-				s = s.substr(4);
-			}
-			if (s.substr(0,5) == "/***/") {
-				nouveau=false;
-				cacher=true;
-				s = s.substr(5);
-			}
-			if (s.substr(0,2) == "/*") {
-				supprimer=true;
-				s = s.substr(2);
-			}
-			s = s.replace(/^[\/\*]+/g, "");
-			var s = s.split(":");
-			if (style[i]=="" || cacher) continue;
-			nbProps++;
-			$bulle.append($(document.createElement('div'))
-				.addClass((nouveau) ? "nouveau" : "vieux")
-				.addClass((supprimer) ? "supprimer" : "")
-				//.addClass((cacher) ? "cacher" : "")
-				.append($(document.createElement('span')).text(s[0]))
-				.append(":")
-				.append($(document.createElement('span')).text(s[1]))
-				.append(";")
-			);
+/*jslint browser:true,esnext:true*/
+class Bullestyle {
+	static ajouterLink() {
+		var link = document.head.appendChild(document.createElement("link"));
+		link.setAttribute("rel", "stylesheet");
+		link.setAttribute("href", "bullestyle.css");
+	}
+	static traiterStyle(style) {
+		if (style instanceof HTMLElement) {
+			style = style.getAttribute("style");
 		}
-		if (nbProps > 0) {
-			$this.addClass("avecbulle").after($bulle);
+		style = style
+			.replace(/^ */g, "")
+			.replace(/ *$/g, "")
+			.replace(/ *: */g, ":")
+			.replace(/ *; */g, ";")
+			.replace(/ *\/\* */g, "/*")
+			.replace(/ *\*\/ */g, "*/")
+			.replace(/;+/g, ";")
+			.split(";");
+		return style;
+	}
+	static propriete(nom, valeur, nouveau, supprimer) {
+		var resultat, span;
+		resultat = document.createElement('div');
+		resultat.classList.add("propriete");
+		resultat.classList.add((nouveau) ? "nouveau" : "vieux");
+
+		if (supprimer) {
+			resultat.classList.add("supprimer");
 		}
-	});
-	$("img.avecbulle").before("<span class='indicateurBulle'></span>");
-});
+
+		span = resultat.appendChild(document.createElement('span'));
+		span.innerHTML = nom;
+		resultat.appendChild(document.createTextNode(":"));
+		span = resultat.appendChild(document.createElement('span'));
+		span.innerHTML = valeur;
+		resultat.appendChild(document.createTextNode(";"));
+		return resultat;
+	}
+	static traiterImagesBulles() {
+		var elements = Array.from(document.querySelectorAll("img.avecbulle"));
+		elements.forEach(element => {
+			var indicateur = element.parentNode.insertBefore(document.createElement("span"), element);
+			indicateur.classList.add("indicateurBulle");
+		});
+	}
+
+	static traiterStyles() {
+		var elements = Array.from(document.querySelectorAll("*[style]"));
+		elements.forEach(element => {
+			var bulle = document.createElement('div');
+			bulle.classList.add("bulle");
+			var style = this.traiterStyle(element);
+			var nouveau = true;
+			var supprimer = false;
+			var cacher = false;
+			var nbProps = 0;
+			for (var i = 0; i < style.length; i++) {
+				var s = style[i];
+				console.log(s);
+				if (s.substr(0, 2) === "*/") {
+					supprimer = false;
+					s = s.substr(2);
+				}
+				while (s.substr(0, 4) === "/**/") {
+					if (i === 0 || nouveau === false) {
+						cacher = true;
+					}
+					nouveau = false;
+					s = s.substr(4);
+				}
+				if (s.substr(0, 5) === "/***/") {
+					nouveau = false;
+					cacher = true;
+					s = s.substr(5);
+				}
+				if (s.substr(0, 2) === "/*") {
+					supprimer = true;
+					s = s.substr(2);
+				}
+				s = s.replace(/^[\/\*]+/g, "");
+				s = s.split(":");
+				if (style[i] !== "" && !cacher) {
+					nbProps++;
+					bulle.appendChild(this.propriete(s[0], s[1], nouveau, supprimer));
+				}
+			}
+			if (nbProps > 0) {
+				element.classList.add("avecbulle");
+				element.parentNode.insertBefore(bulle, element.nextSibling);
+			}
+		});
+	}
+	static load() {
+		this.traiterStyles();
+		this.traiterImagesBulles();
+	}
+	static init() {
+		this.ajouterLink();
+		window.addEventListener("load", () => {
+			this.load();
+		});
+	}
+}
+Bullestyle.init();
